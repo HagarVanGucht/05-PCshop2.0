f2ff4ab0dbd83417a14ff8b19049ad859cd68f88
Hagar Van Gucht <hagar.vangucht@student.romerocollege.be>
2019-10-15 09:59:15 +0200

    Add project files.

---
+++ PCshop2.0/Program.cs
@@ -0,0 +1,80 @@
+﻿//Analyse
+/*vraag het artikel
+ * vraag aantal
+ * vraag eenhiedsprijs
+ *Als onderneming = j
+ *Dan is onderneming = true
+ *Anders is onderneming = false
+ * 
+ */ 
+ //opniew beginnen
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCshop2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Declaraties
+            string input1;
+            ushort input2;
+            decimal input3, output1, output2;
+            bool input4;
+            
+            Console.WriteLine("Artikel: ");
+            input1 = Console.ReadLine();
+
+            Console.WriteLine("Aantal: ");
+            input2 = Console.ReadLine();
+
+            Console.WriteLine("Eenheidsprijs: ");
+            input3 = Console.ReadLine();
+
+            Console.WriteLine("Onderneming j of n: ");
+            input4 = Console.ReadLine();
+
+            //processing...
+            //bool true
+            output1 = input2 * " * " input3 * " * " * (0.21);
+
+            if ( selectievoorwaarde )
+            {
+                sequentie
+            }
+
+
+            //bool false
+            input2 " * " * input3  = " = " output2;
+            
+
+
+
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+    }
+}
